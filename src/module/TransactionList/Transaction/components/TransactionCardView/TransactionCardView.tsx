3e6b30327855ff467176ef5772a88f6b9cf0736f
HarshadAnsorbit <harshad.rathod@ansorbit.com>
2022-12-01 11:12:04 +0530

    fix: transaction amount

--- src/module/TransactionList/Transaction/components/TransactionCardView/TransactionCardView.tsx
+++ src/module/TransactionList/Transaction/components/TransactionCardView/TransactionCardView.tsx
@@ -1,30 +1,46 @@
 import {
   Autocomplete,
   Box,
-  Group,
   NumberInput,
   SimpleGrid,
   Stack,
-  Text,
   TextInput,
   UnstyledButton,
 } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import { UseFormReturnType } from "@mantine/form";
-import { CirclePlus, X } from "tabler-icons-react";
+import { useEffect, useMemo, useRef } from "react";
+import { X } from "tabler-icons-react";
 import { TransactionData } from "../../../../../types";
+import { format } from "../../../../../utils";
 
 export interface TransactionCardViewProps {
   form: UseFormReturnType<TransactionData>;
   areas: string[];
+  index: number;
   handleAddTransaction: () => void;
 }
 
 export const TransactionCardView = ({
   areas,
   form,
+  index,
   handleAddTransaction,
 }: TransactionCardViewProps) => {
+  const transaction = useMemo(() => {
+    return form.values.transactions[index];
+  }, [form.values.transactions, index]);
+
+  const formRef = useRef(form);
+
+  useEffect(() => {
+    const calculateTransactionAmount = transaction.freight + transaction.hamali;
+    formRef.current.setFieldValue(
+      `transactions.${index}.amount`,
+      calculateTransactionAmount || 0
+    );
+  }, [transaction.freight, transaction.hamali, index]);
+
   return (
     <Box
       sx={{
@@ -35,94 +51,88 @@ export const TransactionCardView = ({
       }}
     >
       <Stack>
-        {form.values.transactions.map((element, i) => (
-          <Box
-            key={i}
-            sx={(theme) => ({
-              border: "1px solid",
-              borderColor: theme.colors.gray[4],
-              borderRadius: 4,
-              position: "relative",
-            })}
+        <Box
+          sx={(theme) => ({
+            border: "1px solid",
+            borderColor: theme.colors.gray[4],
+            borderRadius: 4,
+            position: "relative",
+          })}
+        >
+          <UnstyledButton
+            type="button"
+            onClick={() => form.removeListItem("transactions", index)}
+            sx={{ position: "absolute", right: 4, top: 4 }}
           >
-            <UnstyledButton
-              type="button"
-              onClick={() => form.removeListItem("transactions", i)}
-              sx={{ position: "absolute", right: 4, top: 4 }}
-            >
-              <X strokeWidth={1.5} />
-            </UnstyledButton>
-            <SimpleGrid
-              cols={3}
-              key={i}
-              p="sm"
-              breakpoints={[
-                { maxWidth: 980, cols: 2, spacing: "md" },
-                { maxWidth: 755, cols: 2, spacing: "sm" },
-                { maxWidth: 600, cols: 1, spacing: "sm" },
-              ]}
-            >
-              <TextInput
-                placeholder="C. G. No."
-                label="C. G. No."
-                {...form.getInputProps(`transactions.${i}.CGNo`)}
-              />
-              <DatePicker
-                label=" Date"
-                placeholder="Date"
-                withAsterisk
-                {...form.getInputProps(`transactions.${i}.date`)}
-              />
-              <Autocomplete
-                placeholder="From Place"
-                label="From Place"
-                data={areas ?? []}
-                {...form.getInputProps(`transactions.${i}.fromPlace`)}
-                limit={5}
-              />
-              <Autocomplete
-                placeholder="To Place"
-                label="To Place"
-                data={areas ?? []}
-                {...form.getInputProps(`transactions.${i}.toPlace`)}
-                limit={5}
-              />
-              <NumberInput
-                required
-                placeholder="No of Arts"
-                label="No of Arts"
-                {...form.getInputProps(`transactions.${i}.noOfArts`)}
-              />
-              <NumberInput
-                required
-                placeholder="Freight"
-                label="Freight"
-                {...form.getInputProps(`transactions.${i}.freight`)}
-              />
-              <NumberInput
-                required
-                placeholder="Hamali"
-                label="Hamali"
-                {...form.getInputProps(`transactions.${i}.hamali`)}
-              />
-              <NumberInput
-                required
-                placeholder="Amount"
-                label="Amount"
-                {...form.getInputProps(`transactions.${i}.amount`)}
-              />
-            </SimpleGrid>
-          </Box>
-        ))}
-
-        <UnstyledButton type="button" onClick={handleAddTransaction}>
-          <Group spacing={4}>
-            <CirclePlus />
-            <Text weight={600} size="sm">
-              Add New
-            </Text>
-          </Group>
-        </UnstyledButton>
+            <X strokeWidth={1.5} />
+          </UnstyledButton>
+          <SimpleGrid
+            cols={3}
+            key={index}
+            p="sm"
+            breakpoints={[
+              { maxWidth: 980, cols: 2, spacing: "md" },
+              { maxWidth: 755, cols: 2, spacing: "sm" },
+              { maxWidth: 600, cols: 1, spacing: "sm" },
+            ]}
+          >
+            <TextInput
+              placeholder="C. G. No."
+              label="C. G. No."
+              {...form.getInputProps(`transactions.${index}.CGNo`)}
+            />
+            <DatePicker
+              dropdownType="modal"
+              label=" Date"
+              placeholder="Date"
+              withAsterisk
+              inputFormat={format}
+              {...form.getInputProps(`transactions.${index}.date`)}
+            />
+            <Autocomplete
+              placeholder="From Place"
+              label="From Place"
+              data={areas ?? []}
+              {...form.getInputProps(`transactions.${index}.fromPlace`)}
+              limit={5}
+            />
+            <Autocomplete
+              placeholder="To Place"
+              label="To Place"
+              data={areas ?? []}
+              {...form.getInputProps(`transactions.${index}.toPlace`)}
+              limit={5}
+            />
+            <NumberInput
+              hideControls
+              required
+              placeholder="No of Arts"
+              label="No of Arts"
+              {...form.getInputProps(`transactions.${index}.noOfArts`)}
+            />
+            <NumberInput
+              hideControls
+              required
+              placeholder="Freight"
+              label="Freight"
+              {...form.getInputProps(`transactions.${index}.freight`)}
+            />
+            <NumberInput
+              hideControls
+              required
+              placeholder="Hamali"
+              label="Hamali"
+              {...form.getInputProps(`transactions.${index}.hamali`)}
+            />
+            <NumberInput
+              hideControls
+              required
+              placeholder="Amount"
+              label="Amount"
+              {...form.getInputProps(`transactions.${index}.amount`)}
+            />
+          </SimpleGrid>
+        </Box>
       </Stack>
     </Box>
   );
